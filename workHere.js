c21c0018121ff25338e5d06b5210d2cac05961fc
itzel orozco <itzel_oc@hotmail.com>
2019-05-20 08:35:04 -0700

    Ejercicios JS Itzel Orozco
    Signed-off-by: itzel orozco <itzel_oc@hotmail.com>

--- workHere.js
+++ workHere.js
@@ -4,6 +4,96 @@ var billionaires = require('./billionaires.json');
     Get the names of the billionaires that are in their 20s
 */
 
+let twentyYearOldBillionaires = billionaires.filter(element => {
+        if(element.demographics.age>=20&&element.demographics.age<=29){
+            return element.name;
+        };
+    });
+
+/*
+create a new object with the billionairse year, name and worth rounded to the newarest integer
+{
+    year: 1996,
+    name: Bernardo Garza Sada,
+    worth: 2
+}
+*/
+
+
+let roundedWorth = billionaires.map(x =>{
+       return {
+           name: x.name,
+           year:x.year,
+           worth: Math.round(x.wealth['worth in billions'])
+       }      
+    });
+    
+/*
+    sum the rounded worth of all the billionaires
+*/
+
+let aggregatedWorth = billionaires.reduce((prev, curr)=>{
+    return prev + Math.round(curr.wealth['worth in billions']);
+    
+},0);
+
+
+// console.log(aggregatedWorth)
+
+/*
+    find the the richest billionaire, make sure the result is in an array
+*/
+
+let richest = billionaires.reduce((prev, curr)=>{
+    if( Object.keys(prev).length===0 ){
+       return curr;
+    } 
+     if (curr.rank < prev.rank){
+        prev = curr;
+    }   
+    return prev;
+},{});
+richest = [richest];
+console.log(richest);  
+/*
+    find the poorest billioanire, make sure the result is in an array
+*/
+
+let poorest =billionaires.reduce((prev, curr)=>{
+    if( Object.keys(prev).length===0 ){
+       return curr;
+    } 
+     if (curr.rank >= prev.rank){
+        prev = curr;
+    }  
+    return prev;
+},{});
+poorest = [poorest];
+console.log(poorest);  
+/*
+
+/*
+    make a new array containing the richest and the poorest billionaire
+*/
+
+let richAndPoor =  [
+{richest:richest[0]},
+{poorest: poorest[0]}
+];
+
+module.exports = {
+    twentyYearOldBillionaires,
+    roundedWorth,
+    aggregatedWorth,
+    richest,
+    poorest,
+    richAndPoor
+}var billionaires = require('./billionaires.json');
+ 
+/*
+    Get the names of the billionaires that are in their 20s
+*/
+
 let twentyYearOldBillionaires = null;
 
 /*
